3a2fefa56ebd7fdb1ca274ed6e5e0ceac7acd9fb
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-21 20:04:05 +0400

    conditionally mount in components in UI

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
-  <h4>App Component Text</h4>
-  <ChildStyles>
-    <h4>ChildStyles Component Text</h4> 
-  </ChildStyles>
+  <button @click="activeTab = 'TabA'">Tab A</button>
+  <button @click="activeTab = 'TabB'">Tab B</button>
+  <button @click="activeTab = 'TabC'">Tab C</button>
+
+  <TabA v-if="activeTab === 'TabA'"/>
+  <TabB v-if="activeTab === 'TabB'"/>
+  <TabC v-if="activeTab === 'TabC'"/>
 </template>
 
 <script>
-import ChildStyles from "./components/ChildStyles.vue";
+import TabA from "./components/TabA.vue";
+import TabB from "./components/TabB.vue";
+import TabC from "./components/TabC.vue";
 export default {
   name: 'App',
   components: {
-   ChildStyles
+    TabA,
+    TabB,
+    TabC
   },
   data(){
     return {
-
+      activeTab: 'TabA'
     }
   },
   methods: {
---
+++ src/components/TabA.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Tab A conent
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TabA'
+    }
+</script>
+
+<style scoped>
+
+</style>
