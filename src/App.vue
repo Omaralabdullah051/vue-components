c7119440badadb19b56d40ca946bcc65279982c0
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-06-21 14:20:03 +0400

    prop types and validations

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <Greet name="Bruce" hero-name="Batman"/>
+  <!-- <Greet name="Bruce" hero-name="Batman"/>
   <Greet name="Clark" hero-name="Superman"/>
   <Greet name="Michell" hero-name="Ironman"/>
-  <Greet :name="name" :hero-name="heroName"/>
+  <Greet :name="name" :hero-name="heroName"/> -->
+
+  <Article title="Article Title" :likes="50" :isPublished="true" :object="{name:'batman'}"/>
 </template>
 
 <script>
-import Greet from './components/Greet.vue';
+// import Greet from './components/Greet.vue';
+import Article from "./components/Article.vue";
 
 export default {
   name: 'App',
   components: {
-    Greet
+    // Greet,
+    Article,
   },
   data(){
     return {
---
+++ src/components/Article.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+       <h2>Article Component</h2>
+       <h2>{{title}}</h2>
+       <h2>Likes -{{likes}}</h2>
+       <h2>Published -{{ isPublished ? 'Yes' : 'No' }}</h2>
+       <h2>Name - {{object.name}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ArticleA',
+        props: {
+            title: {
+                type: String,
+                required: true,
+            },
+            likes:{
+                type: Number,
+                default: 40
+            },
+            isPublished: Boolean,
+            object: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
